00e5b69e3fbe9973c0d49ec85e4bedd4d43ef17d
Ahmed Gado <ahmedehabg@gmail.com>
2019-12-04 00:46:08 -0500

    add comment for day 1 part 1

--- day1/solve.py
+++ day1/solve.py
@@ -9,4 +9,5 @@ def fuel_with_fuel(mass):
     return 0 if fuel(mass) <= 0 else fuel(mass) + fuel_with_fuel(fuel(mass))
 
 
+# use fuel instead of fuel_with_fuel for part 1
 print(sum((fuel_with_fuel(int(mass)) for mass in open('puzzle_1_input', 'r'))))
